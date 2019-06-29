0342d3a054ba0174440b033222223643aa956bd9
Matthew Glassman <matthewglassman78@gmail.com>
2019-06-29 10:45:05 -0400

    Hook up remove emitter in Dog.vue to activate the binding in App.vue

--- src/components/Dog.vue
+++ src/components/Dog.vue
@@ -5,7 +5,7 @@
   :src="dog"></v-img>
   <v-card-actions>
     <v-spacer></v-spacer>
-      <v-btn icon @click="removeFromFavorites(index)">
+      <v-btn icon @click="$emit('remove')">
         <v-icon>delete</v-icon>
       </v-btn>
   </v-card-actions>
